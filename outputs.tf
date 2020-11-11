f24de0aa92e68b56269648db439fca283bbfa98c
Dmytro <d.ostapenko97@live.com>
2020-11-11 12:19:43 +0200

    Edit output name for elasticsearch (if type is LoadBalancer)

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "namespace" {
 output "kibana_url" {
   value = "kibana.${var.domain}"
 }
-output "elasticserach_url" {
+output "elasticsearch_lb_endpoint" {
   value = var.elasticsearch_service_type == "LoadBalancer" ? module.elasticsearch_service.load_balancer_ingress_hostname : null
 }
